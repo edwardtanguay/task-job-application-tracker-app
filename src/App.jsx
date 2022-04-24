a499b184dde8e1722fe5d445fab2afc060710ff9
Edward Tanguay (windows) <edward@tanguay.info>
2022-04-24 22:22:37 +0200

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@ import './App.scss';
 import jobs from './data/jobs.json';
 
 function App() {
-	const displayKind = 'list';
+	const displayKind = 'full';
 
 	return (
 		<div className="App">
 			<h1>Job Application Process</h1>
-			<div className="jobs">
-				{jobs.map((job, index) => {
-					return (
-						<>
-							{displayKind === 'full' ? (
+			{displayKind === 'full' ? (
+				<div className="jobs">
+					{jobs.map((job, index) => {
+						return (
+							<>
 								<div key={index} className="job">
 									<div className="position">
 										<a href={job.url}>{job.position}</a>
@@ -21,13 +21,23 @@ function App() {
 										{job.bulkText}
 									</div>
 								</div>
-							) : (
-								<li key={index}>list</li>
-							)}
-						</>
-					);
-				})}
-			</div>
+							</>
+						);
+					})}
+				</div>
+			) : (
+				<ul className="jobList">
+					{jobs.map((job, index) => {
+						return (
+							<>
+								<li key={index}>
+									<a href={job.url}>{job.position}</a>
+								</li>
+							</>
+						);
+					})}
+				</ul>
+			)}
 		</div>
 	);
 }
